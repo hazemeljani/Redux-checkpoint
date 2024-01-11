675eb354fcae0c5ea41ed97d6861c6bf009a79aa
hazem <hazemjh10@gmail.com>
2024-01-11 01:54:22 +0100

    first commit

---
+++ src/actions/todoInfo.js
@@ -0,0 +1,5 @@
+export const EMPTY_DONE_TASK =
+  " no tasks done are available, try doing one";
+export const EMPTY_NOT_DONE_TASK =
+   "you did all your tasks, try to add one";
+export const EMPTY_TODO_TASK = " you don't have any tasks!, try adding one";
